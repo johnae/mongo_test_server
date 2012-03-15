a840da5e006062a8bfef18cc40a03fa096ebc6b3
John Axel Eriksson <john@insane.se>
2012-03-15 18:29:04 +0100

    Change how the global server gets configured + specs.

--- lib/mongo_test_server.rb
+++ lib/mongo_test_server.rb
@@ -11,89 +11,101 @@ module MongoTestServer
   class Mongod
   
     class << self
-  
-      $mongo_test_server = nil
-  
-      def configure(port_or_config, name)
-        port = port_or_config.to_i
-        path = nil
-        config = {}
-        if port_or_config.is_a?(String) && File.exists?(port_or_config)
-          if File.exists?(port_or_config)
-            config = YAML.load(ERB.new(File.read(port_or_config)).result)[ENV['RAILS_ENV']]
-            port = config.key?('port') ? config['port'].to_i : 27017
-          end
+
+      def configure(options={}, &block)
+        options.each do |k,v|
+          server.send("#{k}=",v) if server.respond_to?("#{k}=")
         end
-        path = config.key?('path') ? config['path'] : `which mongod`.chomp
-        $mongo_test_server ||= new(port, name, path)
+        yield(server) if block_given?
+      end
+
+      def server
+        @mongo_test_server ||= new
       end
   
       def start_server
-        unless $mongo_test_server.nil?
-          $mongo_test_server.start
+        unless @mongo_test_server.nil?
+          @mongo_test_server.start
         else
           puts "MongoTestServer not configured properly!"
         end
       end
   
       def stop_server
-        unless $mongo_test_server.nil?
-          $mongo_test_server.stop
+        unless @mongo_test_server.nil?
+          @mongo_test_server.stop
         end
       end
   
     end
 
-    attr_accessor :port
-    attr_accessor :path
-    attr_reader :mongo_dir
-    attr_reader :mongo_log
+    attr_writer :port
+    attr_writer :path
+    attr_writer :name
     attr_reader :mongo_instance_id
   
-    def initialize(port, name, path)
-      @port = port
-      @path = path
-      @name = name
+    def initialize(port=nil, name=nil, path=nil)
+      self.port = port
+      self.path = path
+      self.name = name
       @mongo_process_or_thread = nil
       @mongo_instance_id = "#{Time.now.to_i}_#{Random.new.rand(10000000000..90000000000)}"
-      @mongo_dir = "/tmp/#{name}_mongo_testserver_#{@mongo_instance_id}"
-      @mongo_log = "#{@mongo_dir}/mongo.log"
       @oplog_size = 200
       @configured = true
       self.started = false
     end
 
+    def mongo_log
+      "#{mongo_dir}/mongo_log"
+    end
+
+    def port
+      @port ||= 27017
+    end
+
+    def path
+      @path ||= `which mongod`.chomp
+    end
+
+    def name
+      @name || "#{Random.new.rand(10000000000..90000000000)}"
+    end
+
+    def mongo_dir
+      @mongo_dir ||= "/tmp/#{self.name}_mongo_testserver_#{@mongo_instance_id}"
+    end
+
     def mongo_cmd_line
-      "#{@path} --port #{@port} --dbpath #{@mongo_dir} --noprealloc --nojournal --noauth --nohttpinterface --nssize 1 --oplogSize #{@oplog_size} --smallfiles --logpath #{@mongo_log}"
+      "#{self.path} --port #{self.port} --dbpath #{self.mongo_dir} --noprealloc --nojournal --noauth --nohttpinterface --nssize 1 --oplogSize #{@oplog_size} --smallfiles --logpath #{self.mongo_log}"
     end
   
     def prepare
-      FileUtils.rm_rf @mongo_dir
-      FileUtils.mkdir_p @mongo_dir
+      FileUtils.rm_rf self.mongo_dir
+      FileUtils.mkdir_p self.mongo_dir
     end
   
     def started?
-      File.directory?(@mongo_dir) && File.exists?("#{@mongo_dir}/started")
+      File.directory?(self.mongo_dir) && File.exists?("#{self.mongo_dir}/started")
     end
 
     def killed?
-      !File.directory?(@mongo_dir) || File.exists?("#{@mongo_dir}/killed")
+      !File.directory?(self.mongo_dir) || File.exists?("#{self.mongo_dir}/killed")
     end
 
     def started=(running)
-      if File.directory?(@mongo_dir)
-        running ? FileUtils.touch("#{@mongo_dir}/started") : FileUtils.rm_f("#{@mongo_dir}/started")
+      if File.directory?(self.mongo_dir)
+        running ? FileUtils.touch("#{self.mongo_dir}/started") : FileUtils.rm_f("#{self.mongo_dir}/started")
       end
     end
 
     def killed=(killing)
-      if File.directory?(@mongo_dir)
-        killing ? FileUtils.touch("#{@mongo_dir}/killed") : FileUtils.rm_f("#{@mongo_dir}/killed")
+      if File.directory?(self.mongo_dir)
+        killing ? FileUtils.touch("#{self.mongo_dir}/killed") : FileUtils.rm_f("#{self.mongo_dir}/killed")
       end
     end
 
     def error?
-      File.exists?("#{@mongo_dir}/error")
+      File.exists?("#{self.mongo_dir}/error")
     end
   
     def configured?
@@ -101,7 +113,6 @@ module MongoTestServer
     end
   
     def start
-      #puts "Starting mongod: #{mongo_cmd_line}"
       unless started?
         prepare
         if RUBY_PLATFORM=='java'
@@ -124,14 +135,13 @@ module MongoTestServer
       unless killed? || $?.success?
         error_message = <<-ERROR
           <#{self.class.name}> Error executing command: #{command}
-          <#{self.class.name}> Result is: #{IO.binread(@mongo_log)}
+          <#{self.class.name}> Result is: #{IO.binread(self.mongo_log)}
           <#{self.class.name}> Error is: #{File.read(error_filepath)}
         ERROR
-        File.open("#{@mongo_dir}/error", 'w') do |f|
+        File.open("#{self.mongo_dir}/error", 'w') do |f|
           f << error_message
         end
         self.killed=true
-        #raise Exception.new, error_message
       end
       result
     end
@@ -140,7 +150,7 @@ module MongoTestServer
       retries = 10
       begin
         self.started = true
-        c = Mongo::Connection.new("localhost", @port)
+        c = Mongo::Connection.new("localhost", self.port)
         c.close
       rescue Exception => e
         if retries>0 && !killed? && !error?
@@ -152,7 +162,7 @@ module MongoTestServer
           error_lines = []
           error_lines << "<#{self.class.name}> cmd was: #{mongo_cmd_line}"
           error_lines << "<#{self.class.name}> ERROR: Failed to connect to mongo database: #{e.message}"
-          IO.binread(@mongo_log).split("\n").each do |line|
+          IO.binread(self.mongo_log).split("\n").each do |line|
             error_lines << "<#{self.class.name}> #{line}"
           end
           stop
@@ -162,7 +172,7 @@ module MongoTestServer
     end
   
     def pids
-      pids = `ps ax | grep mongod | grep #{@port} | grep #{@mongo_dir} | grep -v grep | awk '{print \$1}'`.chomp
+      pids = `ps ax | grep mongod | grep #{self.port} | grep #{self.mongo_dir} | grep -v grep | awk '{print \$1}'`.chomp
       pids.split("\n").map {|p| (p.nil? || p=='') ? nil : p.to_i }
     end
   
@@ -171,12 +181,12 @@ module MongoTestServer
       self.killed = true
       self.started = false
       mongo_pids.each { |ppid| `kill -9 #{ppid} 2> /dev/null` }
-      FileUtils.rm_rf @mongo_dir
+      FileUtils.rm_rf self.mongo_dir
       self
     end
 
     def mongoid_options(options={})
-      options = {host: "localhost", port: @port, database: "#{@name}_test_db", use_utc: false, use_activesupport_time_zone: true}.merge(options)
+      options = {host: "localhost", port: self.port, database: "#{self.name}_test_db", use_utc: false, use_activesupport_time_zone: true}.merge(options)
     end
   
     def mongoid_yml(options={})
--- spec/mongo_test_server/mongo_test_server_spec.rb
+++ spec/mongo_test_server/mongo_test_server_spec.rb
@@ -29,6 +29,12 @@ describe MongoTestServer::Mongod do
       subject.started?.should be_true
     end
 
+    it "should not complain if starting a started mongod" do
+      lambda { subject.start }.should_not raise_error
+      subject.started?.should be_true
+      lambda { subject.start }.should_not raise_error
+    end
+
     it "should raise if something else is listening on the same port" do
       server_same_port.start
       subject.start.should raise_error
@@ -81,6 +87,42 @@ describe MongoTestServer::Mongod do
       File.directory?(subject.mongo_dir).should be_true
       subject.stop
       File.exists?(subject.mongo_dir).should be_false
+    end
+
+  end
+
+  context "MongoTestServer#configure" do
+
+    let(:name) { "somename" }
+    let(:port) { 33221 }
+    let(:path) { `which mongod`.chomp }
+
+    subject do
+      MongoTestServer::Mongod.configure do |server|
+        server.name = name
+        server.port = port
+        server.path = path
+      end
+      MongoTestServer::Mongod.server
+    end
+
+    it "should configure a global mongod" do
+      subject.start
+      subject.started?.should be_true
+      subject.port.should == port
+      subject.name.should == name
+      subject.path.should == path
+    end
+
+    it "should configure a global mongod using options instead of block" do
+      MongoTestServer::Mongod.instance_variable_set(:@mongo_test_server, nil)
+      MongoTestServer::Mongod.configure(port: (port+1), name: "someothername", path: path)
+      s = MongoTestServer::Mongod.server
+      s.start
+      s.started?.should be_true
+      s.port.should == (port+1)
+      s.name.should == "someothername"
+      s.path.should == path
     end
 
   end
