577ee25a6c8bb6db1573c8f9ddb666f142b2a9cd
John Axel Eriksson <john@insane.se>
2012-11-28 12:57:05 +0100

    Get ram_disk config from mongoid.yml test env.

--- lib/mongo_test_server/railtie.rb
+++ lib/mongo_test_server/railtie.rb
@@ -9,10 +9,12 @@ module MongoTestServer
         config_file = File.expand_path(File.join(Rails.root, 'config', 'mongoid.yml'))
         mongoid_options = YAML.load(ERB.new(File.read(config_file)).result)[ENV['RAILS_ENV']]
         port = mongoid_options['sessions']['default']['hosts'].first.split(':').last.to_i
+        use_ram_disk = !!mongoid_options['use_ram_disk']
         MongoTestServer::Mongod.configure do |server|
           server.name = app_name
           server.port = port
           server.path = `which mongod`.chomp
+          server.use_ram_disk = use_ram_disk
         end
         MongoTestServer::Mongod.server.start
       end
