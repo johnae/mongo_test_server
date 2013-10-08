dbbf23e5afb1455bf752a3193922c279b1c262bc
John Axel Eriksson <john@insane.se>
2013-10-08 13:46:04 +0200

    Also run in if env==ci

--- lib/mongo_test_server/railtie.rb
+++ lib/mongo_test_server/railtie.rb
@@ -3,7 +3,7 @@ require 'rails'
 module MongoTestServer
   class MongoTestServerRailtie < ::Rails::Railtie #:nodoc:
     config.before_initialize do
-      if Rails.env.test?
+      if Rails.env.test? || Rails.env.ci?
         $stderr.puts "MongoTestServer: starting..."
         app_name = ENV['APP_NAME'] || Rails.application.class.to_s.split("::").first.downcase
         config_file = File.expand_path(File.join(Rails.root, 'config', 'mongoid.yml'))
